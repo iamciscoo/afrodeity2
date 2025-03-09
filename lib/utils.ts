9a88fa7ecc30f4e308e629194331ba7adb55a028
iamciscoo <francisjac21@gmail.com>
2025-03-09 12:32:26 +0300

    Remove currency-related code from utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,17 +1,6 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import { type Currency } from "@/store/use-currency"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
-
-export function formatPrice(price: number, currency: Currency) {
-  const convertedPrice = price * currency.rate
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: currency.code,
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2,
-  }).format(convertedPrice)
 }
